b089763f1a7d2c35a49afd461aca355be43d0467
alexichen <alexichen@tencent.com>
2020-11-17 19:05:38 +0800

    完成tabbar

--- douyin/dyapp/src/components/home/TabBar.vue
+++ douyin/dyapp/src/components/home/TabBar.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="tab-bar">
-    <div class="item"><span class="active">首页</span></div>
-    <div class="item"><span>熟人</span></div>
-    <div class="item"><span>+</span></div>
-    <div class="item"><span>消息</span></div>
-    <div class="item"><span>我</span></div>
+    <div class="item" @click="changeTab(0)">
+      <span :class="tabIndex == 0 ? 'active' : ''">首页</span>
+    </div>
+    <div class="item" @click="changeTab(1)">
+      <span :class="tabIndex == 1 ? 'active' : ''">熟人</span>
+    </div>
+    <div class="item" @click="changeTab(2)">
+      <span :class="tabIndex == 2 ? 'active' : ''"
+        ><img class="dy-btn" src="../../static/img/dy-btn.png" alt=""
+      /></span>
+    </div>
+    <div class="item" @click="changeTab(3)">
+      <span :class="tabIndex == 3 ? 'active' : ''">消息</span>
+    </div>
+    <div class="item" @click="changeTab(4)">
+      <span :class="tabIndex == 4 ? 'active' : ''">我</span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tabIndex: 0,
+    };
+  },
+  methods: {
+    changeTab(index) {
+      this.tabIndex = index;
+      console.log(this.tabIndex);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -24,15 +48,25 @@ export default {};
   color: #808080;
   font-size: 16px;
   display: flex;
-  justify-items:center;
+  justify-items: center;
 }
 
 .tab-bar .item {
-    flex : 1;
-    text-align: center;
+  flex: 1;
+  text-align: center;
 }
 
 .tab-bar .item .active {
-    color : white;
+  color: white;
+}
+
+.tab-bar .item span {
+  padding: 10px 0;
+}
+
+.tab-bar .item .dy-ttn {
+  width: 30px;
+  height: 30px;
+  padding: 10px;
 }
 </style>
